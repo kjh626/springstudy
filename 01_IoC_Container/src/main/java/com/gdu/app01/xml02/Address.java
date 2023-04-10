c20b31b418bdd57fabdb7cddfc98a51ec24e5b35
kjh626 <wjd1113@naver.com>
2023-04-10 16:26:24 +0900

    2023-04-10

---
+++ 01_IoC_Container/src/main/java/com/gdu/app01/xml02/Address.java
@@ -0,0 +1,25 @@
+package com.gdu.app01.xml02;
+
+public class Address {
+	private String doromyung;
+	private String jibeon;
+	private Contact contact;
+	public String getDoromyung() {
+		return doromyung;
+	}
+	public void setDoromyung(String doromyung) {
+		this.doromyung = doromyung;
+	}
+	public String getJibeon() {
+		return jibeon;
+	}
+	public void setJibeon(String jibeon) {
+		this.jibeon = jibeon;
+	}
+	public Contact getContact() {
+		return contact;
+	}
+	public void setContact(Contact contact) {
+		this.contact = contact;
+	}
+}
