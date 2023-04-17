ac26549ba36aeda0d26c5eb50f33de79fd6aa173
kjh626 <wjd1113@naver.com>
2023-04-17 16:50:07 +0900

    2023-04-17

--- 04_Jdbc_Junit/src/main/java/com/gdu/app04/repository/BoardDAO.java
+++ 04_Jdbc_Junit/src/main/java/com/gdu/app04/repository/BoardDAO.java
@@ -51,21 +51,98 @@ public class BoardDAO {
 	// 1. 목록
 	public List<BoardDTO> selectBoardList() {
 		List<BoardDTO> list = new ArrayList<BoardDTO>();
-		list.add(new BoardDTO(1, "제목", "내용", "작성자", "작성일", "수정일"));
+		try {
+			con = getConnection();
+			sql = "SELECT BOARD_NO, TITLE, CONTENT, WRITER, CREATED_AT, MODIFIED_AT FROM BOARD ORDER BY BOARD_NO DESC";
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				BoardDTO board = new BoardDTO(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6));
+				list.add(board);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
 		return list;
 	}
 	
 	// 2. 상세
-	
+	public BoardDTO selectBoardByNo(int board_no) {
+		BoardDTO board = null;
+		try {
+			con = getConnection();
+			sql = "SELECT BOARD_NO, TITLE, CONTENT, WRITER, CREATED_AT, MODIFIED_AT FROM BOARD WHERE BOARD_NO = ?";
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, board_no);
+			rs = ps.executeQuery();
+			if(rs.next()) {
+				board = new BoardDTO(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6));
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return board;
+	}
 	
 	// 3. 삽입
-	
+	public int insertBoard(BoardDTO board) {
+		int result = 0;
+		try {
+			con = getConnection();
+			sql = "INSERT INTO BOARD VALUES(BOARD_SEQ.NEXTVAL, ?, ?, ?, TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS'), TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS'))";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, board.getTitle());
+			ps.setString(2, board.getContent());
+			ps.setString(3, board.getWriter());
+			result = ps.executeUpdate();
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return result;
+	}
 	
 	// 4. 수정
+	public int updateBoard(BoardDTO board) {
+		int result = 0;
+		try {
+			con = getConnection();
+			sql = "UPDATE BOARD SET TITLE = ?, CONTENT = ?, MODIFIED_AT = TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS') WHERE BOARD_NO = ?";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, board.getTitle());
+			ps.setString(2, board.getContent());
+			ps.setInt(3, board.getBoard_no());
+			result = ps.executeUpdate();
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return result;
+	}
 	
 	
 	// 5. 삭제
-	
+	public int deleteBoard(int board_no) {
+		int result = 0;
+		try {
+			con = getConnection();
+			sql = "DELETE FROM BOARD WHERE BOARD_NO = ?";
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, board_no);
+			result = ps.executeUpdate();
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return result;
+	}
 	
 	
 	
